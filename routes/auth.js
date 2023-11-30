eea64cca383297f2ce4d92a249f3a0002dabb47e
alamayomekonen <alamayo274@gmail.com>
2023-11-29 18:41:01 -0800

    working on the Authentication

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const { check, body } = require("express-validator");
+
 const authController = require("../controllers/auth");
 
 const router = express.Router();
@@ -10,7 +12,33 @@ router.get("/signup", authController.getSignup);
 
 router.post("/login", authController.postLogin);
 
-router.post("/signup", authController.postSignup);
+router.post(
+  "/signup",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("please enter a valid email address")
+      .custom((value, { req }) => {
+        if (value === "test2@test.com") {
+          throw new Error("this email address is Forbiden.");
+        }
+        return true;
+      }),
+    body(
+      "password",
+      "Please enter a password with only numbers and text and at least 5 characters"
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+    body("confirmPassword").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords have to match!");
+      }
+      return true;
+    }),
+  ],
+  authController.postSignup
+);
 
 router.post("/logout", authController.postLogout);
 
