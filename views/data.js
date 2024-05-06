f4b4ffa28c9ea7816d88f4a4d1bb68eecbbc173c
adiabil <User@almayos-mekonen-MacBook-Air.local>
2024-05-06 20:24:51 +0300

    products

---
+++ views/data.js
@@ -0,0 +1,32 @@
+const Products = [
+  {
+    title: "Electric bikes on sale",
+    imageUrl: "/productimages/bike.jpeg",
+    price: 5299,
+    description:
+      "🚲 Discover eco-friendly electric bikes at unbeatable prices! Enjoy unmatched performance and go green with our limited-time mega sale. Get expert support and exclusive discounts – shop now and revolutionize your ride! 🌟",
+  },
+  {
+    title: "iPhone 15 on sale",
+    imageUrl: "path/to/image2.jpg",
+    price: 29.99,
+    description:
+      "📱 Elevate your tech game with the sleek and powerful iPhone 15. Experience cutting-edge features, stunning design, and unparalleled performance. Don't miss out on our exclusive launch offer – upgrade to the future today! 🚀",
+  },
+  {
+    title: "Sony 5 on sale",
+    imageUrl: "path/to/image3.jpg",
+    price: 39.99,
+    description:
+      "Experience the pinnacle of entertainment with the Sony 5. Immerse yourself in crystal-clear visuals, superior sound quality, and seamless connectivity. Elevate your media experience to new heights with Sony 5.",
+  },
+  {
+    title: "",
+    imageUrl: "path/to/image3.jpg",
+    price: 39.99,
+    description:
+      "Dive into the world of immersive virtual reality with our cutting-edge VR technology. Transport yourself to new dimensions, explore exciting virtual environments, and engage in thrilling experiences like never before. Unlock a world of endless possibilities with our VR headset.",
+  },
+];
+
+module.exports = Products;
